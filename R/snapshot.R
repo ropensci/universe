5b937ea8cbd609776c9a85cc0510f5431e6214f5
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-10-14 15:18:48 +0200

    chore: paste ok

--- R/snapshot.R
+++ R/snapshot.R
@@ -37,8 +37,8 @@ download_single_repo <- function(url, destdir, type = "src") {
     return(df)
   }
   writeLines(sprintf("Mirror from %s at %s", url, as.character(Sys.time())), "timestamp.txt") # nolint: line_length_linter
-  df[["fileurl"]] <- paste0(url, "/", pkg_file(df[["Package"]], df[["Version"]], type))
-  pkgfiles <- paste0(url, "/", c("PACKAGES", "PACKAGES.gz", "PACKAGES.rds"))
+  df[["fileurl"]] <- paste0(url, "/", pkg_file(df[["Package"]], df[["Version"]], type)) # nolint: paste_linter
+  pkgfiles <- paste0(url, "/", c("PACKAGES", "PACKAGES.gz", "PACKAGES.rds")) # nolint: paste_linter
   results <- curl::multi_download(c(pkgfiles, df$fileurl))
   unlink(results$destfile[results$status_code != 200])
   outfiles <- basename(df$fileurl)
